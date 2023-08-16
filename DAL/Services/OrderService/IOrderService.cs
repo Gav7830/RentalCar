58523997716bb439495955d993daa6e8fe12da75
Gavriel D <emailgavriel@gmail.com>
2023-08-16 19:59:50 +0300

    first commit

---
+++ DAL/Services/OrderService/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+
+namespace DAL.Services
+{
+    internal interface IOrderService
+    {
+        public Task<string> PostOrder(Order order);
+        public Task<object> GetOrders(int userId);
+    }
+}
